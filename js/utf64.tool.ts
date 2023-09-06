3b5e94a0f18cbf29eeafe58ca9c480c441cdc004
Iain Merrick <iain@moreplease.com>
2023-09-06 11:51:55 +0100

    Update utf64 tool to allow inline input as well as stdin

--- js/utf64.tool.ts
+++ js/utf64.tool.ts
@@ -4,9 +4,12 @@ import * as fs from "node:fs";
 
 import { str_to_utf64, utf64_to_str } from "./utf64.js";
 
-const USAGE = `Usage: utf64 [-hd]
+const USAGE = `Usage: utf64 [-hd] [args...]
   -h, --help   Display this message
-  -d, --decode Decode input
+  -d, --decode Decode input (default is encode)
+
+Each argument is (de)coded and printed to a separate line on stdout.
+If no arguments are supplied, stdin is used for input.
 `;
 
 const args = [...process.argv];
@@ -14,8 +17,13 @@ args.shift(); // node
 args.shift(); // utf64.tool.js
 
 let decode = false;
+const input = [];
 
 for (let arg = args.shift(); arg; arg = args.shift()) {
+  if (!arg.startsWith("-")) {
+    input.push(arg);
+    continue;
+  }
   switch (arg) {
     case "-h":
     case "--help":
@@ -34,7 +42,11 @@ for (let arg = args.shift(); arg; arg = args.shift()) {
   }
 }
 
-const input = fs.readFileSync(0, "utf-8");
-const output = decode ? utf64_to_str(input) : str_to_utf64(input);
+if (input.length === 0) {
+  input.push(fs.readFileSync(0, "utf-8"));
+}
 
-process.stdout.write(`${output}\n`);
+for (const line of input) {
+  const output = decode ? utf64_to_str(line) : str_to_utf64(line);
+  process.stdout.write(`${output}\n`);
+}

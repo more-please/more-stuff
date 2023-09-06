66388932958a367fea3e2cd83a66115796d85ce5
Iain Merrick <iain@moreplease.com>
2023-09-06 12:38:19 +0100

    Add --version flag to tool (surprisingly fiddly)

--- js/utf64.tool.ts
+++ js/utf64.tool.ts
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
 import * as fs from "node:fs";
+import * as path from "node:path";
+import * as url from "node:url";
 
 import { str_to_utf64, utf64_to_str } from "./utf64.js";
 
-const USAGE = `Usage: utf64 [-hd] [args...]
-  -h, --help   Display this message
-  -d, --decode Decode input (default is encode)
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const USAGE = `Usage: utf64 [-dhv] [args...]
+  -d, --decode  Decode input (default is encode)
+  -h, --help    Display this message
+  -v, --version Display version number
 
 Each argument is (de)coded and printed to a separate line on stdout.
 If no arguments are supplied, stdin is used for input.
@@ -25,15 +30,25 @@ for (let arg = args.shift(); arg; arg = args.shift()) {
     continue;
   }
   switch (arg) {
+    case "-d":
+    case "--decode":
+      decode = true;
+      break;
+
     case "-h":
     case "--help":
       process.stdout.write(USAGE);
       process.exit(0);
 
-    case "-d":
-    case "--decode":
-      decode = true;
-      break;
+    case "-v":
+    case "--version": {
+      // Read manually instead of importing, as Node currently
+      // requires import assertion but prints a warning anyway.
+      const src = path.join(__dirname, "../package.json");
+      const json = JSON.parse(fs.readFileSync(src, "utf-8"));
+      process.stdout.write(`${json.version}\n`);
+      process.exit(0);
+    }
 
     default:
       process.stderr.write(`*** Unknown flag: ${arg}\n\n`);

6e0f7a81e5703c3f413d794d2ac7fc471da1b6d3
Iain Merrick <iain@moreplease.com>
2023-09-26 13:11:57 +0100

    Refactoring: extract server code (need to use a src symlink as Deno doesn't understand file: dependencies, sigh)

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@
 
 import { Command, Option } from "@commander-js/extra-typings";
 
-import { gosub } from "./gosub.ts";
+import { gosub } from "./lib/index.ts";
 
 const { base, port } = new Command()
   .description("Standalone Gosub server")

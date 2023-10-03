3f64780e4765b13553bc8d6e49b08b0f98434d6d
Iain Merrick <iain@moreplease.com>
2023-10-03 16:20:18 +0100

    Add website WIP note

--- website/src/assets.ts
+++ website/src/assets.ts
@@ -1,4 +1,5 @@
 import github_mark_svg from "../assets/github-mark.svg?url";
 import gosub_svg from "../assets/gosub.svg?url";
+import under_construction from "../assets/under-construction.svg?url";
 
-export { github_mark_svg, gosub_svg };
+export { github_mark_svg, gosub_svg, under_construction };
--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { gosub_svg } from "../assets.ts";
+import { gosub_svg, under_construction } from "../assets.ts";
 
 export default function Home() {
   return (
@@ -7,6 +7,18 @@ export default function Home() {
         <img src={gosub_svg} alt="GOSUB" width="236" height="64" />
       </h1>
       <h2>Serve Go modules from a GitHub repo subdirectory</h2>
+      <img
+        src={under_construction}
+        alt="Under construction"
+        width="64"
+        height="64"
+      />
+      <p>Under construction!</p>
+      <p>
+        {"Here’s the "}
+        <a href="https://github.com/more-please/gosub-goproxy">GitHub repo</a>
+        {" if you just want the code."}
+      </p>
     </main>
   );
 }

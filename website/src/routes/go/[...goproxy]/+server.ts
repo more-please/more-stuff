4da6327e776012392742a9f5283e17ca88eeab6a
Iain Merrick <iain@moreplease.com>
2023-11-02 15:50:01 +0000

    Use GITHUB_TOKEN from environment

--- website/src/routes/go/[...goproxy]/+server.ts
+++ website/src/routes/go/[...goproxy]/+server.ts
@@ -1,12 +1,18 @@
 import { error, type RequestHandler } from "@sveltejs/kit";
 import { goproxy } from "gosub-goproxy";
 
-const handler = goproxy("/go", {
+const config = {
   url: "https://github.com/more-please/utf64",
   module: "utf64.moreplease.com",
   directory: "go",
   tagPrefix: "go-",
-});
+};
+
+const env = {
+  githubToken: process.env.GITHUB_TOKEN,
+};
+
+const handler = goproxy("/go", config, env);
 
 export const GET: RequestHandler = async (event) => {
   const response = await handler(event.request);

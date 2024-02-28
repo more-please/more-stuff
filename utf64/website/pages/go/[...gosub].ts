56ad0853d88e55caf50dbc81211197d9451272a8
Iain Merrick <iain@moreplease.com>
2024-02-28 11:13:14 +0000

    Fix gosub server for utf64

---
+++ utf64/website/pages/go/[...gosub].ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+import { goproxy } from "gosub-goproxy";
+
+const config = {
+  url: "https://github.com/more-please/more-stuff",
+  module: "utf64.moreplease.com",
+  directory: "utf64/go",
+  tagPrefix: "utf64-go-",
+};
+
+const handler = goproxy("/go", config);
+
+export const GET: APIRoute = async ({ request }) => {
+  const response = await handler(request);
+  if (!response) {
+    return new Response("Not Found", { status: 404});
+  }
+  return response;
+};

d671f3ae5159fb1724f1660c7f393a08cae4fb27
Iain Merrick <iain@moreplease.com>
2024-03-06 11:37:49 +0000

    Auto-formatting

--- gosub/website/pages/github.com/[...gosub].ts
+++ gosub/website/pages/github.com/[...gosub].ts
@@ -6,7 +6,7 @@ const handler = gosub("/");
 export const GET: APIRoute = async ({ request }) => {
   const response = await handler(request);
   if (!response) {
-    return new Response("Not Found", { status: 404});
+    return new Response("Not Found", { status: 404 });
   }
   return response;
 };
--- utf64/website/pages/go/[...goproxy].ts
+++ utf64/website/pages/go/[...goproxy].ts
@@ -13,7 +13,7 @@ const handler = goproxy("/go", config);
 export const GET: APIRoute = async ({ request }) => {
   const response = await handler(request);
   if (!response) {
-    return new Response("Not Found", { status: 404});
+    return new Response("Not Found", { status: 404 });
   }
   return response;
 };

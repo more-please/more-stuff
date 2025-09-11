92f18f8e44fe6def4e87b070851aff993649f787
Iain Merrick <iain@moreplease.com>
2025-09-11 14:09:49 +0100

    Fix formatting

--- gosub/src/index.ts
+++ gosub/src/index.ts
@@ -1,3 +1,3 @@
-export type { GoproxyEnv, GoproxyOptions, GoproxyConsole} from "./types.ts";
+export type { GoproxyEnv, GoproxyOptions, GoproxyConsole } from "./types.ts";
 export { goproxy } from "./goproxy.ts";
 export { gosub, gosubEncode, gosubDecode } from "./gosub.ts";

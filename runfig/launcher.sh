4fb16c7a993c136c8ad05319f97f4cc541b3a8e4
Iain Merrick <iain@moreplease.com>
2026-02-05 12:38:46 +0000

    Remove runfig launch script, just use Node 22's native TS support

--- runfig/launcher.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SELF="$(readlink -f -- "${0}")"
-ROOT="$(dirname -- "${SELF}")"
-
-TSX="${ROOT}/node_modules/.bin/tsx"
-
-exec ${TSX} "${ROOT}/runfig.ts" "${@}"
--- runfig/runfig.ts
+++ runfig/runfig.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env tsx
+#!/usr/bin/env node
 
 import * as TOML from "smol-toml";
 import * as YAML from "yaml";

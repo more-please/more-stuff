249497bde18d653075019b495968644f8db7d00e
Iain Merrick <iain@moreplease.com>
2026-03-19 12:53:50 +0000

    Use tsup for rollup-plugin-build-info

--- rollup-plugin-build-info/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { BuildInfo } from "./build-info";
-
-export default function plugin(): {
-  name: string;
-  buildStart(): void;
-  resolveId(source: string): string | null;
-  load(id: string): Promise<string | null>;
-};
